6cbf4cd365a03b5bd02b752b3e56973fc048be8e
feng <fengsight@gmail.com>
2025-03-22 21:50:18 +0900

    Add dist

---
+++ packages/excalidraw/dist/dev/data/image-XNFNIMYC.js
@@ -0,0 +1,13 @@
+import {
+  decodePngMetadata,
+  encodePngMetadata,
+  getTEXtChunk
+} from "../chunk-BD3TN2F4.js";
+import "../chunk-66VA7UC4.js";
+import "../chunk-XDFCUUT6.js";
+export {
+  decodePngMetadata,
+  encodePngMetadata,
+  getTEXtChunk
+};
+//# sourceMappingURL=image-XNFNIMYC.js.map
