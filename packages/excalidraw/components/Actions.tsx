277766dc1933416a5c1ae92c6b7b89d59f8446bb
feng <fengsight@gmail.com>
2026-01-26 01:00:46 +0900

    fix: issues

--- packages/excalidraw/components/Actions.tsx
+++ packages/excalidraw/components/Actions.tsx
@@ -746,6 +746,10 @@ const CombinedExtraActions = ({
                       {renderAction("alignBottom")}
                       {targetElements.length > 2 &&
                         renderAction("distributeVertically")}
+                      {/* breaks the row ˇˇ */}
+                      <div style={{ flexBasis: "100%", height: 0 }} />
+                      {renderAction("arrangeElements")}
+                      {renderAction("normaliseElements")}
                     </div>
                   </div>
                 </fieldset>
