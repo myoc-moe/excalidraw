dba13d6235f2a78ec086039e1886ddd39f322557
feng <fengsight@gmail.com>
2026-05-25 19:13:15 +0900

    fix: add missing max

--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -328,6 +328,7 @@ export const DEFAULT_MAX_IMAGE_WIDTH_OR_HEIGHT = 1440;
 export const MAX_ALLOWED_FILE_BYTES = 100 * 1024 * 1024; // Myoc file up to 100MB
 
 export const DEFAULT_IMAGE_OPTIONS: AppProps["imageOptions"] = {
+  maxWidthOrHeight: DEFAULT_MAX_IMAGE_WIDTH_OR_HEIGHT,
   maxFileSizeBytes: MAX_ALLOWED_FILE_BYTES,
 };
 
